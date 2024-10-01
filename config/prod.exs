e9b41d4326567d521d9d39e5fd3a9e09c8f4800a
ProgMastermind <surya.21mis7126@vitapstudent.ac.in>
2024-10-01 15:07:00 +0530

    added change

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,6 @@
 import Config
 
-config :redis,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+config :logger, level: :info
+
+config :elixircache,
+  server_port: {:system, "PORT"}
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,17 +1,9 @@
 import Config
 
-config :redis, port: String.to_integer(System.get_env("PORT") || "4000")
-config :redis, websocket_port: String.to_integer(System.get_env("WS_PORT") || "3001")
-config :logger, level: :info
-
+# config :redis, port: String.to_integer(System.get_env("PORT") || "4000")
+# config :redis, websocket_port: String.to_integer(System.get_env("WS_PORT") || "3001")
+# config :logger, level: :info
 if config_env() == :prod do
-
-  host = System.get_env("PHX_HOST" || "elixircache.gigalixir.com")
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
-  config :redis,
-      http:[
-        ip: {0,0,0,0,0,0,0,0},
-          port: port
-      ]
+  port = System.get_env("PORT") || "4000"
+  config :elixircache, server_port: String.to_integer(port)
 end
