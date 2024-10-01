162bd00c7473dbf3820a015ee16f8f6a158895a7
ProgMastermind <surya.21mis7126@vitapstudent.ac.in>
2024-10-01 15:02:40 +0530

    added changes

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,7 +10,6 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :redis,
-    url : [host: host, port: 443, scheme: "https"],
       http:[
         ip: {0,0,0,0,0,0,0,0},
           port: port
